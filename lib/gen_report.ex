d9502d97705ded0bc8b8319bb36250e7988b851d
Cristiano Aro de Oliveira <cristiano_sx@hotmail.com>
2021-09-07 14:50:55 -0300

    Chapter II - Challenge II - Using paralelism to solving

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -23,6 +23,51 @@ defmodule GenReport do
     parse_response(all_hours, hours_per_month, hours_per_year)
   end
 
+  def build_many(filenames) when not is_list(filenames), do: {:error, "Insert list file!"}
+
+  def build_many(filenames) do
+    filenames
+    |> Task.async_stream(&build(&1))
+    |> Enum.reduce(
+      %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}},
+      fn {:ok, result}, report ->
+        sum_reports(result, report)
+      end
+    )
+  end
+
+  defp sum_reports(result, report) do
+    %{
+      "all_hours" => all_hours1,
+      "hours_per_month" => hours_per_month1,
+      "hours_per_year" => hours_per_year1
+    } = result
+
+    %{
+      "all_hours" => all_hours2,
+      "hours_per_month" => hours_per_month2,
+      "hours_per_year" => hours_per_year2
+    } = report
+
+    all_hours = merge_maps(all_hours1, all_hours2)
+
+    hours_per_month =
+      Map.merge(hours_per_month1, hours_per_month2, fn _key, value1, value2 ->
+        Map.merge(value1, value2, fn _key, value1, value2 -> value1 + value2 end)
+      end)
+
+    hours_per_year =
+      Map.merge(hours_per_year1, hours_per_year2, fn _key, value1, value2 ->
+        Map.merge(value1, value2, fn _key, value1, value2 -> value1 + value2 end)
+      end)
+
+    parse_response(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
   defp sum_all_hours(parsed_file) do
     parsed_file
     |> Enum.reduce(
